d3d6003d72fcd2be793c5052a2de80b959b1c521
piercarlo schiavo <piercarlo.schiavo@fidatek.it>
2025-06-29 08:29:44 +0200

    Bash modified with dynamic torch port

--- melo/train.sh
+++ melo/train.sh
@@ -1,14 +1,18 @@
 CONFIG=$1
 GPUS=$2
+LOG_DIR=$3
 MODEL_NAME=$(basename "$(dirname $CONFIG)")
 
 PORT=10902
+while ! nc -z localhost $PORT; do
+  PORT=$((PORT + 1))
+done
 
 while : # auto-resume: the code sometimes crash due to bug of gloo on some gpus
 do
 torchrun --nproc_per_node=$GPUS \
         --master_port=$PORT \
-    train.py --c $CONFIG --model $MODEL_NAME --log_dir $LOG_DIR
+    train.py --c $CONFIG --model $MODEL_NAME --log_dir $LOG_DIR | tee $LOG_DIR/train.log | while read line; do echo "$line"; done
 
 for PID in $(ps -aux | grep $CONFIG | grep python | awk '{print $2}')
 do
